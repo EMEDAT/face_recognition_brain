d9b25c5d5ebedec90426b5ad7afd1495c5d66a6d
Emediong David Titus <emedattitus24@gmail.com>
2024-04-11 05:43:18 +0100

    Update Rank component to center align the text and increase the padding

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,10 +3,10 @@ import React from "react";
 const Rank = () => {
     return (
         <div>
-            <div className="white f3 tc">
+            <div className="white f3 tc ph3">
                 {'Emedat, your current rank is ...'}
             </div>
-            <div className="white f1">
+            <div className="white f1 tc ph4">
                 {'#5'}
             </div>
         </div>
